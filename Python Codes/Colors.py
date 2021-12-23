6db4495a65883fa6c8abbed4b12686a94452e710
Bhavya Sehgal <85365197+bhavyassehgal@users.noreply.github.com>
2021-12-23 18:22:53 +0530

    colors for formatting output on the console

---
+++ Python Codes/Colors.py
@@ -0,0 +1,12 @@
+# this class contains colors for formatting output on the console
+class colors:
+    PINK = '\033[95m'
+    LIGHT_RED = '\033[31m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    BLUE = '\033[34m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
